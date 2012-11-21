807c2049e42c16d5922de54b248cb4d9c25bed00
Hashcode <hashcode0f@gmail.com>
2012-11-21 14:34:25 -0800

    razr-m changes
    Change-Id: I489c20f8b0983fef242ed6faf985666db71718b7

--- sbin/changeslot.sh
+++ sbin/changeslot.sh
@@ -1,11 +1,15 @@
 #!/sbin/bbx sh
 # By: Hashcode
-# Last Editted: 11/07/2012
+# Last Editted: 11/19/2012
 SS_SLOT=${1}
 BLOCK_DIR=/dev/block
 BLOCKNAME_DIR=$BLOCK_DIR/platform/msm_sdcc.1/by-name
 SS_MNT=/ss
 SS_DIR=$SS_MNT/safestrap
+
+/sbin/bbx umount /system
+/sbin/bbx umount /data
+/sbin/bbx umount /cache
 
 rm $BLOCKNAME_DIR/system
 rm $BLOCKNAME_DIR/userdata
